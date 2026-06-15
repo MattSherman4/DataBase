b749bbff6d984302f98547ea0eac1b3b48cba7c0
MattSherman4 <mattsherman2001@gmail.com>
2026-06-14 20:37:01 -0400

    Got a names config *celebration emoji*

--- _Draft_CFG_Maker/Code/NameOdds.py
+++ _Draft_CFG_Maker/Code/NameOdds.py
@@ -1,7 +1,13 @@
 from Load_Data import *
 from Load_Variables import *
+import configparser
 
 def setNameOdds():
+    DEFAULT_NORMAL = 1
+    DEFAULT_SPECIAL = 0
+    INCREASE_PER_INSTANCE = 1
+
+
     history_draft = load_draft_all()
     history_draft = history_draft[history_draft['Year'] >= MODERN_ERA].copy()
 
@@ -29,29 +35,33 @@ def setNameOdds():
     #  Fills the first_count and first_hyphen_total variables, calculating statistical weights for first names
     def first_name(s, first_count, first_hyphen_total):
         if '-' not in s:
+            s = check_special(s)
             # Non-hyphenated names default to a weight of 2 to lessen the chance of a non-unique hyphenated name
-            first_count[s] = first_count.get(s, 99) + 50
+            first_count[s] = first_count.get(s, DEFAULT_NORMAL) + INCREASE_PER_INSTANCE
         else:
             first_hyphen_total += 1
             #  Seperate hyphenated names since they are not last names
             s = s.split('-')
-            first_count[s[0]] = first_count.get(s[0], 0) + 50
-            first_count[s[-1]] = first_count.get(s[-1], 0) + 50
+            for name in s:
+                name = check_special(name)
+                first_count, first_hyphen_total = last_name(name, first_count, first_hyphen_total)
         return first_count, first_hyphen_total
 
     #_ Data for a last name _#
     #  Fills the last_count and last_hyphen_total variables, calculating statistical weights for last names
     def last_name(s, last_count, last_hyphen_total):
         if '-' not in s:
+            s = check_special(s)
             # Non-hyphenated names default to a weight of 2 to lessen the chance of a non-unique hyphenated name
-            last_count[s] = last_count.get(s, 99) + 50
+            last_count[s] = last_count.get(s, DEFAULT_NORMAL) + INCREASE_PER_INSTANCE
         else:
             last_hyphen_total += 1
             #  Keep hyphenated names together for story line puropses
-            last_count[s] = last_count.get(s, 0) + 50
+            last_count[s] = last_count.get(s, DEFAULT_SPECIAL) + INCREASE_PER_INSTANCE
             #  Also split the name and use both halves independantly in the dictionary 
             s = s.split('-')
             for name in s:
+                name = check_special(name)
                 last_count, last_hyphen_total = last_name(name, last_count, last_hyphen_total)
         return last_count, last_hyphen_total
 
@@ -61,44 +71,63 @@ def setNameOdds():
         if s not in generational_suffixes:
             last_count, last_hyphen_total = last_name(s, last_count, last_hyphen_total)
             return suffix_count, suffix_total, last_count, last_hyphen_total
+        s = check_special(s)
         suffix_total += 1
-        suffix_count[s] = suffix_count.get(s, 0) + 50
+        suffix_count[s] = suffix_count.get(s, DEFAULT_SPECIAL) + INCREASE_PER_INSTANCE
         return suffix_count, suffix_total, last_count, last_hyphen_total
-
-    # #_ Quick fix for str title error _#
-    # #  Special title to fix str.title()'s apostrophe problem
-    # def special_title(s:str):
-    #     s = s.title()
-    #     return s
 
     #_ Quick fix for name grammar error _#
     #  Changes special names
     #TODO: Combine this with util_Data
-    def check_special(s:str, first_count, last_count, suffix_count):
+    def check_special(s:str):
         if len(s) > 2 and s[-2] == "'":
             s = s[:-1] + s[-1].lower()
         if s[:2] == 'Mc':
             return 'Mc' + s[2:].title()
         if s in special:
             if s == 'J':
-                return random_name(0, first_count, last_count, suffix_count)
+                return ''
             elif s == 'K':
-                return random_name(0, first_count, last_count, suffix_count)
+                return ''
             elif s == 'Lequint':
                 return 'LeQuint'
-            elif s == 'Rock':
-                return random_name(0, first_count, last_count, suffix_count)
-        return s        
+        if s in Upper:
+            return s.upper()
+        return s
+    
+    first_count = {}
+    first_hyphen_total = 0
+    last_count = {}
+    last_hyphen_total = 0
+    suffix_count = {}
+    suffix_total = 0
 
-    #  Will re-spin the name if there is more than one hyphen
-    def check_hyphenated_grammar(arr, i, first_count, last_count, suffix_count):
-        ret = []
-        #  Avoid using any more than one hyphen
-        for name in arr:
-            while "-" in name:
-                name = random_name(i, first_count, last_count, suffix_count)
-            ret.append(name)
-        # Avoid using names that end in an apostrophe as teh first numae in a hyphenated name
-        if ret[0][-1] == "'":
-            ret[0] = ret[0][:-1]
-        return ret
+    first_count, first_hyphen_total, last_count, last_hyphen_total, suffix_count, suffix_total = name_data(history_draft, first_count, first_hyphen_total, last_count, last_hyphen_total, suffix_count, suffix_total)
+
+    try:
+        del first_count['']
+    except:
+        pass
+    try:
+        del last_count['']
+    except:
+        pass
+    try:
+        del suffix_count['']
+    except:
+        pass
+
+    total = float(len(history_draft))
+    first_hyphen_percentage = round((first_hyphen_total / total) * 10.0, 5)
+    last_hyphen_percentage = round((last_hyphen_total / total) * 10.0, 5)
+    suffix_percentage = round((suffix_total / total) * 10.0, 5)
+
+    config = configparser.ConfigParser()
+    config["Percentages"] = {"first_hyphen_percentage" : first_hyphen_percentage,
+                             "last_hyphen_percentage" : last_hyphen_percentage,
+                             "suffix_percentage" : suffix_percentage}
+    config["Names"] = {"first_count" : first_count, "last_count" : last_count, "suffix_count" : suffix_count}
+    with open("C:/Users/pensh/Desktop/VSCode/DataBase/_Draft_CFG_Maker/Data/names.cfg", "w", encoding = "utf-8") as configfile:
+        config.write(configfile)
+
+setNameOdds()   
